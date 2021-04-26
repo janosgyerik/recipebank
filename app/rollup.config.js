d78e35bce1acb4184422d65177cd3430bb6784c8
Janos Gyerik <info+kraken@janosgyerik.com>
2021-04-26 12:13:05 +0200

    Rename main.js to app.js

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -9,7 +9,7 @@ import css from 'rollup-plugin-css-only';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
+	input: 'src/app.js',
 	output: {
 		sourcemap: true,
 		format: 'iife',
